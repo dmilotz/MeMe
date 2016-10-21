c37b1fd77d0b2d61cdd9a903093e6bf46d051ba9
Dirk Milotz <dirkmilotz@dirks-macbook-pro.local>
2016-10-20 18:02:55 -0700

    Initial Commit

---
+++ ImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by Dirk Milotz on 10/20/16.
+//  Copyright © 2016 Dirk Milotz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
