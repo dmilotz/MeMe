5a2f81849deb48d24e960ea866504b233de61bd3
Dirk Milotz <dirkmilotz@dirks-macbook-pro.local>
2016-10-23 13:12:03 -0700

    Added share feature

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -16,14 +16,73 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     var bottomTextCleared = false
     var topTextCleared = false
+    var memeImage : UIImage!
+    
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    
+    @IBAction func share(_ sender: AnyObject) {
+        memeImage = generateMemedImage()
+        let controller = UIActivityViewController(activityItems: [memeImage], applicationActivities: nil)
+        
+        self.present(controller, animated: true, completion: nil)
+        
+        controller.completionWithItemsHandler = {(activityType, completed, returnedItems, error) in
+            
+            // Return if cancelled
+            if (!completed) {
+                return
+            }
+            self.save()
+            controller.dismiss(animated: true, completion: nil)
+            
+            
+        }
+    }
+  
+
+    
+    struct Meme {
+        let botText: String
+        let topText : String
+        let originalImage: UIImage
+        let memeImage: UIImage
+        
+   }
+    
+    func save(){
+       let meme = Meme(botText: bottomText.text!, topText: topText.text!, originalImage:imagePicker.image!, memeImage: memeImage )
+    }
+    
+    
+    func generateMemedImage() -> UIImage
+    {
+        self.navigationController?.navigationBar.isHidden = true
+        self.navigationController?.isToolbarHidden = true
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame,
+                                     afterScreenUpdates: true)
+        let memedImage : UIImage =
+            UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        self.navigationController?.navigationBar.isHidden = false
+        self.navigationController?.isToolbarHidden = false
+        
+        return memedImage
+    }
+    
     
     
     let memeTextAttributes = [
         NSStrokeColorAttributeName : UIColor .black,
         NSForegroundColorAttributeName : UIColor .white,
         NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSStrokeWidthAttributeName : 10.0
+        NSStrokeWidthAttributeName : -1.0
     ] as [String : Any]
+    
+    
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
         if textField.tag == 1 && !topTextCleared{
@@ -34,6 +93,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             bottomText.text = ""
             bottomTextCleared = true
         }
+       
         
     }
     
@@ -88,12 +148,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         topText.defaultTextAttributes = memeTextAttributes
         topText.delegate = self
         topText.textAlignment = .center
-        
-        
-
-        
-        
-        
+        shareButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -103,7 +158,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
+        
         self.subscribeToKeyboardNotifications()
     }
 
@@ -116,6 +173,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         controller.sourceType = UIImagePickerControllerSourceType.photoLibrary
         self.present(controller, animated: true, completion: nil)
        self.imagePickerController(controller, didFinishPickingMediaWithInfo: ["Blah":"Blah"])
+        
         
     }
     
@@ -133,10 +191,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+       
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
             imagePicker.image = image
             picker.dismiss(animated: true, completion: nil)
         }
+         shareButton.isEnabled = true
+        
+        
     
         
     }
